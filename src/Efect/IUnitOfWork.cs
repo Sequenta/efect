5623a53be996122f673931bb3bc4b02cd9ca9cc5
Mihail Ivanov <tranquilized.mind@gmail.com>
2016-08-27 21:55:59 +0600

    Unit of work

---
+++ src/Efect/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Efect.Fetching;
+
+namespace Efect
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>(IFetcher<TEntity> fetcher) where TEntity : class;
+        Task SaveChangesAsync();
+    }
+}
---
+++ src/Efect/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Efect.Fetching;
+using Microsoft.EntityFrameworkCore;
+
+namespace Efect
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected readonly DbContext Context;
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>(IFetcher<TEntity> fetcher) where TEntity : class
+        {
+            return new Repository<TEntity>(Context, fetcher);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
